0e42ead7a111ebf8bb23c8b34e54aebe9321e108
Papacharlie1 <97809890+Papacharlie1@users.noreply.github.com>
2022-01-28 18:20:05 +0100

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,42 @@
-pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+import "./Ownable.sol";
+contract Bank is Ownable {
+    
+    mapping(address => uint) balances;
+    bool private _paused;
+    
+    constructor () {
+        _paused = false;
+    }
+    
+    //allow to execute when contract is not paused
+    modifier whenNotPaused() {
+        require(!_paused);
+        _;
+    }
+    
+    modifier whenPaused() {
+        require(_paused);
+        _;
+    }
+    
+    function pause() public onlyOwner whenNotPaused {
+        _paused = true;
+    }
+    
+    function unPause() public onlyOwner whenPaused {
+        _paused = false;
+    }
+    
+    function withdrawAll() public whenNotPaused { 
+    uint amountToWithdraw = 
+    balances[msg.sender];
+    balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amountToWithdraw);
+    }
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
-
-contract YourContract {
-
-  // event SetPurpose(address sender, string purpose);
-
-  string public purpose = "Building Unstoppable Apps!!!";
-
-  constructor() payable {
-    // what should we do on deploy?
-  }
-
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      // emit SetPurpose(msg.sender, purpose);
-  }
-
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    function emergencyWithdrawal(address emergencyAddress) public onlyOwner whenPaused {
+        //withdrawal to owner
+    }
+    
 }
